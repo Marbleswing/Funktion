c3829b1ced60e7ffa4bdf346f94e74a842c339ba
Marbleswing <marbleswing9051@gmail.com>
2024-12-14 17:14:24 +0100

    v1.0

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
 const trommel = 1;
 const kabel = 0.12;
-let gulash = true;
 
 function Gewicht_berechnen(meter, einheit){
     const ergebnis = meter * kabel + trommel;
-    if(einheit == "kg"){
-        return ergebnis;
-    } else if(einheit == "t") {
+    if(einheit == "G" || einheit == "g"){
+        return ergebnis * 1000;
+    } else if(einheit == "T" || einheit == "t") {
         return ergebnis / 1000;
     } else {
-        return ergebnis * 1000;
+        return ergebnis;
     }
 }
 
 $(document).ready(function () {
     $("#berechnen").on("click", function() {
-        if (gulash){
-            $("#einheit_text").text("Gulash");
-            gulash = false;
-        } else{
-            const länge = $('#länge').val();
-            const einheit = $('#einheit').val();
-            $("#einheit_text").text(einheit);
-            $("#ergebnis").text(Gewicht_berechnen(länge, einheit));
-        }
-        
+        const länge = $('#länge').val();
+        const einheit = $('#einheit').val();
+        $("#einheit_text").text(einheit);
+        $("#ergebnis").text(Gewicht_berechnen(länge, einheit));
     });
 });
